704fbdc8d568c5220e478a8f819eaa4e6bf02709
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-03-31 13:57:00 +0300

    Update CrowdControl.sh

--- CrowdControl.sh
+++ CrowdControl.sh
@@ -1,6 +1,6 @@
-sudo apt -q update
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
 sudo apt -qy install curl git jq lz4 build-essential
-sudo apt -qy upgrade
 sudo apt install make clang pkg-config lz4 libssl-dev build-essential git jq ncdu bsdmainutils htop -y
 sudo apt install curl -y
 
@@ -13,35 +13,32 @@ echo 'export GO111MODULE=on' >> $HOME/.bash_profile
 echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
 go version
 
-cd && rm -rf Cardchain
-git clone https://github.com/DecentralCardGame/Cardchain
-cd Cardchain
-git checkout v0.14.2
-
-make install
+cd $HOME
+wget -O Cardchaind https://github.com/DecentralCardGame/Cardchain/releases/download/v0.14.2/Cardchaind
+chmod +x Cardchaind
+sudo mv Cardchaind /usr/local/bin
 
 go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@latest
 
+Cardchaind config node tcp://localhost:${CARDCHAIN_PORT}657
+Cardchaind config keyring-backend os
 Cardchaind config chain-id cardtestnet-10
-Cardchaind config keyring-backend test
-Cardchaind config node tcp://localhost:26657
-
 echo -e Your Node Name
 read MONIKER
 Cardchaind init "$MONIKER" --chain-id cardtestnet-10
 
-curl -L https://snapshots-testnet.nodejumper.io/cardchain-testnet/genesis.json > $HOME/.cardchaind/config/genesis.json
-curl -L https://snapshots-testnet.nodejumper.io/cardchain-testnet/addrbook.json > $HOME/.cardchaind/config/addrbook.json
+wget -O $HOME/.cardchaind/config/genesis.json https://testnet-files.itrocket.net/cardchain/genesis.json
+wget -O $HOME/.cardchaind/config/addrbook.json https://testnet-files.itrocket.net/cardchain/addrbook.json
 
-sed -i -e 's|^seeds *=.*|seeds = "2aa407243c982ce2d9ee607b15418cf45b5002f7@202.61.225.157:20056,947aa14a9e6722df948d46b9e3ff24dd72920257@cardchain-testnet-seed.itrocket.net:31656"|' $HOME/.cardchaind/config/config.toml
-
-sed -i -e 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.01ubpf"|' $HOME/.cardchaind/config/app.toml
-
-sed -i \
-  -e 's|^pruning *=.*|pruning = "custom"|' \
-  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
-  -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
-  $HOME/.cardchaind/config/app.toml
+SEEDS="947aa14a9e6722df948d46b9e3ff24dd72920257@cardchain-testnet-seed.itrocket.net:31656"
+PEERS="99dcfbba34316285fceea8feb0b644c4dc67c53b@cardchain-testnet-peer.itrocket.net:31656,cb9977720002f48f2130ee4b4e32bcd48e8548db@89.117.53.20:12456,646e8daa511beae341fb74ce787fe97c4352cc1e@185.249.227.91:656,d4b441e1806fdefb6011eb4417baef9972ef2195@89.117.52.33:12456,f26a4837e525b5c18790e9084e635732279c6e7b@75.119.134.140:26656,d0ac4b03425c4ca22f31ef78121b42119704b2b4@161.97.157.109:31656,feaea5cc2231ffa8f8531aab946b5d68bff18615@89.117.53.86:12456,d0e4edcdd73a7578b10980b3739a5b7218b7e86f@212.23.222.109:26256,b5c51c6284e5093fe05bdbab14422a1c58bf7526@212.22.70.9:36656,7fab831a2ae8c67badd1c7463afb7430c4f0ef05@142.132.202.86:16001,ca809647d5d73ef9247e94df133b1fd40ccce827@144.217.68.182:26656"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.cardchaind/config/config.toml
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.cardchaind/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.cardchaind/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"50\"/" $HOME/.cardchaind/config/app.toml
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0.0ubpf"|g' $HOME/.cardchaind/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.cardchaind/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.cardchaind/config/config.toml
 
 #CHECK PORTS
 PORT=333
@@ -90,21 +87,28 @@ if ss -tulpen | awk '{print $5}' | grep -q ":9090$" ; then
 fi
 
 
-sudo tee /etc/systemd/system/Cardchaind.service > /dev/null << EOF
+sudo tee /etc/systemd/system/Cardchaind.service > /dev/null <<EOF
 [Unit]
-Description=CrowdControl node service
+Description=Cardchain node
 After=network-online.target
 [Service]
 User=$USER
-ExecStart=$(which Cardchaind) start
+WorkingDirectory=$HOME/.cardchaind
+ExecStart=$(which Cardchaind) start --home $HOME/.cardchaind
 Restart=on-failure
-RestartSec=10
+RestartSec=5
 LimitNOFILE=65535
 [Install]
 WantedBy=multi-user.target
 EOF
-sudo systemctl daemon-reload
-sudo systemctl enable Cardchaind.service
 
-sudo systemctl start Cardchaind.service
-sudo journalctl -u Cardchaind -f --no-hostname -o cat
+Cardchaind tendermint unsafe-reset-all --home $HOME/.cardchaind
+if curl -s --head curl https://testnet-files.itrocket.net/cardchain/snap_cardchain.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
+  curl https://testnet-files.itrocket.net/cardchain/snap_cardchain.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.cardchaind
+    else
+  echo no have snap
+fi
+
+sudo systemctl daemon-reload
+sudo systemctl enable Cardchaind
+sudo systemctl restart Cardchaind && sudo journalctl -u Cardchaind -f
