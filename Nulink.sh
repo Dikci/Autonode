c54dc332c81412ca9f729db1485f98e5462fe211
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-04-12 19:06:15 +0300

    Update Nulink.sh

--- Nulink.sh
+++ Nulink.sh
@@ -17,27 +17,6 @@ ufw enable
 
 sudo apt-get update -q
 
-sudo apt-get install -qy ca-certificates curl gnupg
-
-sudo install -m 0755 -d /etc/apt/keyrings
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update -q
-
-sudo apt-get install -qy docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-sudo systemctl start docker
-
-echo -e "${GREEN}The preparation for the installation of the node has been successfully completed${NC}"
-
 wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.10.23-d901d853.tar.gz
 
 tar -xvzf geth-linux-amd64-1.10.23-d901d853.tar.gz
