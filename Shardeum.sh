48f5adc9bc62290f83cff95a4e30eef3bafb6c24
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-04-16 01:03:33 +0300

    Update Shardeum.sh

--- Shardeum.sh
+++ Shardeum.sh
@@ -29,8 +29,4 @@ exit
 
 cd $HOME
 
-apt install screen
-
-screen -S monitor
-
-wget -q -O node_control.sh https://raw.githubusercontent.com/mesahin001/shardeum/main/node_control.sh && chmod +x node_control.sh && sudo /bin/bash node_control.sh
+screen -d -m -S monitor bash -c 'wget -q -O node_control.sh https://raw.githubusercontent.com/mesahin001/shardeum/main/node_control.sh && chmod +x node_control.sh && sudo /bin/bash node_control.sh'
