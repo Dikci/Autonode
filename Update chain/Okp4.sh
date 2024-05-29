5fcd75ca20821a29fab2c97a2858e017557bdf5f
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-05-29 18:57:31 +0300

    Update Okp4.sh

--- Update chain/Okp4.sh
+++ Update chain/Okp4.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-sudo systemctl stop okp4d
-
-cd || return
-rm -rf okp4d
-git clone https://github.com/okp4/okp4d.git
-cd okp4d || return
-git checkout v7.0.0
-make install
-okp4d version # 7.0.0
-
-sudo systemctl start okp4d
+no update
