91bfc2021ca55a4462037420082aad8c12e030c5
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-05-29 18:53:33 +0300

    Update Ojo.sh

--- Update version/Ojo.sh
+++ Update version/Ojo.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-пока обновлений нет
+cd $HOME
+rm -rf ojo
+git clone https://github.com/ojo-network/ojo.git
+cd ojo
+git checkout  v0.3.0-rc4
+make build
+sudo mv $HOME/ojo/build/ojod $(which ojod)
+sudo systemctl stop ojod
+
+cp $HOME/.ojo/data/priv_validator_state.json $HOME/.ojo/priv_validator_state.json.backup
+
+ojod tendermint unsafe-reset-all --home $HOME/.ojo --keep-addr-book
+curl https://snapshots-testnet.nodejumper.io/ojo-testnet/ojo-testnet_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.ojo
+
+mv $HOME/.ojo/priv_validator_state.json.backup $HOME/.ojo/data/priv_validator_state.json
+sudo systemctl restart ojod
