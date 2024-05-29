2c047abd907cd5bcd03c86ac2f1ce82158aab882
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-05-29 18:49:36 +0300

    Update EntryPoint.sh

--- Update version/EntryPoint.sh
+++ Update version/EntryPoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-нет обновлений
+cd $HOME
+wget -O entrypointd https://github.com/entrypoint-zone/testnets/releases/download/v1.3.0/entrypointd-1.3.0-linux-amd64
+chmod +x entrypointd
+sudo mv $HOME/entrypointd $(which entrypointd)
+sudo systemctl restart entrypointd
