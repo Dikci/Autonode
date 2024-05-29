9d71d6f27deda6965c2010568dcd71002a24bb10
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-05-29 18:47:56 +0300

    Update Warden Protocol.sh

--- Update chain/Warden Protocol.sh
+++ Update chain/Warden Protocol.sh
@@ -9,4 +9,4 @@ unzip $HOME/download/wardend_Linux_x86_64.zip
 rm -rf $HOME/download/wardend_Linux_x86_64.zip
 chmod +x $HOME/download/wardend
 sudo mv $HOME/download/wardend $(which wardend)
-sudo systemctl restart wardend && sudo journalctl -u wardend -f
+sudo systemctl restart wardend
