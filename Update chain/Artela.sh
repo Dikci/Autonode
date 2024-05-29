46d5bba6595ab62813e705542912ef5232a56c02
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-05-29 18:47:31 +0300

    Update Artela.sh

--- Update chain/Artela.sh
+++ Update chain/Artela.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-no update
+cd $HOME
+rm -rf artela
+git clone https://github.com/artela-network/artela
+cd artela
+git checkout v0.4.7-rc6
+make install
+sed -E 's/^pool-size[[:space:]]*=[[:space:]]*[0-9]+$/apply-pool-size = 10\nquery-pool-size = 30/' ~/.artelad/config/app.toml > ~/.artelad/config/temp.app.toml && mv ~/.artelad/config/temp.app.toml ~/.artelad/config/app.toml
+sudo systemctl restart artelad
