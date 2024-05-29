7aa39dcd75c70a7b61323c8b69cf91646365d7d8
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-05-29 18:46:04 +0300

    Update Warden Protocol.sh

--- Update chain/Warden Protocol.sh
+++ Update chain/Warden Protocol.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-нет обновления
+cd $HOME
+rm -rf $HOME/download
+mkdir $HOME/download
+cd $HOME/download
+wget https://github.com/warden-protocol/wardenprotocol/releases/download/v0.3.0/wardend_Linux_x86_64.zip
+unzip $HOME/download/wardend_Linux_x86_64.zip
+rm -rf $HOME/download/wardend_Linux_x86_64.zip
+chmod +x $HOME/download/wardend
+sudo mv $HOME/download/wardend $(which wardend)
+sudo systemctl restart wardend && sudo journalctl -u wardend -f
