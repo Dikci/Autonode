95ab0e436f7e8fd714467bd800a900d838749acd
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-05-29 18:50:49 +0300

    Update Elys.sh

--- Update version/Elys.sh
+++ Update version/Elys.sh
@@ -6,4 +6,13 @@ cd elys
 git checkout v0.32.0
 make install
 
+sudo systemctl stop elysd
+
+cp $HOME/.elys/data/priv_validator_state.json $HOME/.elys/priv_validator_state.json.backup
+
+elysd tendermint unsafe-reset-all --home $HOME/.elys --keep-addr-book
+curl https://snapshots-testnet.nodejumper.io/elys-testnet/elys-testnet_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.elys
+
+mv $HOME/.elys/priv_validator_state.json.backup $HOME/.elys/data/priv_validator_state.json
+
 sudo systemctl restart elysd
