f25b118ec99628024d172a47756c79706910469b
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-05-29 18:43:24 +0300

    Update Soarchain.sh

--- Update chain/Soarchain.sh
+++ Update chain/Soarchain.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-Нет обновления
+sudo systemctl stop soarchaind
+
+curl -s https://raw.githubusercontent.com/soar-robotics/testnet-binaries/main/v0.2.9/ubuntu22.04/soarchaind > soarchaind
+chmod +x soarchaind
+sudo mv -f soarchaind "$(which soarchaind)"
+
+sudo systemctl start soarchaind
