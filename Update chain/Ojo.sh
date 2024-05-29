f2a81adf362e9c8e86dc302d0bab1753155096c7
Mr Dikci <97982648+Dikci@users.noreply.github.com>
2024-05-29 18:46:36 +0300

    Update Ojo.sh

--- Update chain/Ojo.sh
+++ Update chain/Ojo.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-нет обновы сети
+cd $HOME
+rm -rf ojo
+git clone https://github.com/ojo-network/ojo.git
+cd ojo
+git checkout  v0.3.0-rc4
+make build
+sudo mv $HOME/ojo/build/ojod $(which ojod)
+sudo systemctl restart ojod && sudo journalctl -u ojod -f
